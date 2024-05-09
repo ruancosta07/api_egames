e1d6da8a5da1b81efe31dc8dc8591809ca453ffc
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-09 15:24:50 -0300

    ajuste nas urls permitidas

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "http://localhost:5173/"],
+    // origin: ["http://localhost:3000/", "http://localhost:5173/"],
     // credentials: true,
   })
 );
