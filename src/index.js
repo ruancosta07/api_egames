29696a633969014a070a12170d025d8e75dc9c16
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-14 17:33:02 -0300

    utilização do compression

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const cors = require("cors");
 const usuario = require("./routes/usuario");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-require('dotenv').config()
-const sessionKey = process.env.SESSION_KEY
+const compression = require("compression");
+require("dotenv").config();
+const sessionKey = process.env.SESSION_KEY;
+app.use(compression({ level: 3 }));
 app.use(cookieParser());
 app.use(
   session({
@@ -21,8 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: ["http://localhost:3000/", "http://localhost:5173/"],
-    // credentials: true,
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
   })
 );
 app.use("/", produtos);
