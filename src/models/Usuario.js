8a371c74a7c8512c85e3d45cf597e506d06af569
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-16 22:39:04 -0300

    Model de usuários para o mongodb

---
+++ src/models/Usuario.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+const Usuario = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select:false
+    },
+    role:{
+        type: String,
+        required: true,
+        default: 'client'
+    },
+    adress:[],
+    cart: []
+})
+
+Usuario.pre('save', async function(next){
+    if(this.isModified('password')) this.password = await bcrypt.hash(this.password, 12)
+        next()
+})
+
+module.exports = mongoose.model('usuarios', Usuario)
