cea8bfa62f67bdc4404d089f3c6edd63fe3c05de
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-16 22:37:01 -0300

    configuração do bucket do supabase

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,13 +1,6 @@
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 const supabaseProducts = createClient(supabaseUrl, supabaseServiceKey);
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-const supabaseConfig = {
-    supabaseBase: supabase,
-    supabaseProducts
-}
-
-module.exports = supabaseConfig
+module.exports = supabaseProducts
