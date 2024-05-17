cb3f06d450ade71b9944b410daa04a67ced4763f
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-16 22:37:51 -0300

    middleware de autenticação de usuário

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const jwtKey = process.env.JWT_KEY
+function authMidleware(req, res, next) {
+    const {authorization} = req.headers;
+
+    if(!authorization){
+        return res.status(401).json({error: 'Token não encontrado'})
+    }
+    const [, token] = authorization.split(' ')
+    try {
+        const decoded = jwt.verify(token, `${jwtKey}`)
+        // res.json({token: decoded})
+        next()
+    } catch (error) {
+        return res.status(401).json({error: 'Token inválido'})
+    }
+}
+
+module.exports = authMidleware;
