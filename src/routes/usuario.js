7b6716d369695bdbfda40d299c2ab15ec3f6eea8
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-05-16 22:41:19 -0300

    troca da lógica de autenticação de criação de usuário para o mongodb

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,51 +1,66 @@
 const usuario = require("express").Router();
-const { supabaseProducts } = require("../database/connect");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Usuario = require("../models/Usuario");
 require("dotenv").config();
 const jwtKey = process.env.JWT_KEY;
+
 usuario.post("/criar-conta", async (req, res) => {
   const { name, email, password } = req.body;
-  const { data, error } = await supabaseProducts
-    .from("usuarios")
-    .insert([{ name, email, password: await bcrypt.hash(password, 10) }]);
-  if (error && error.code == "23505")
-    return res.status(400).json({ message: "Esse email já está cadastrado." });
-    return res.status(200).json({ message: "Usuário criado com sucesso!" });
+  const erros = {};
+  if (!name) {
+    erros.name = "Nome é obrigatório";
+  }
+  if (!email) {
+    erros.email = "Email é obrigatório";
+  }
+  if (!password) {
+    erros.password = "Senha é obrigatória";
+  }
+  if (erros.name || erros.email || erros.password) {
+    return res.status(400).json(erros);
+  } else {
+    const findUser = await Usuario.findOne({ email });
+    if (!!findUser === true) {
+      res.status(401).json({ error: "Esse email já está cadastrado" });
+    } else {
+      const novoUsuario = { name, email, password };
+      try {
+        const userSalvo = await new Usuario(novoUsuario).save();
+        if (!!userSalvo === true) {
+          res.json({ message: "Usuário criado com sucesso" });
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
 });
 
 usuario.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const { data, error } = await supabaseProducts
-    .from("usuarios")
-    .select("*")
-    .match({ email });
-
-    if(data){
-    try{
-      const hashPassword = data[0].password;
-    const result = await bcrypt.compare(password, hashPassword);
-    if (!result) return res.status(400).send("Senha incorreta");
-    else {
-      const userToken = jwt.sign({ email, role: "client" }, `${jwtKey}`, {
-        expiresIn: "20m",
-      });
-      req.session.userToken = userToken;
-      // res.cookie("userToken", userToken, {
-      //   secure: false,
-      //   maxAge: 20 * 60 * 1000,
-      //   httpOnly: false,
-      //   sameSite: 'none',
-      //   path: "",
-      // });
-      res.status(200).json({ message: "Login efetuado com sucesso", cookie: userToken });
-    }
-    }
-    catch{
-      res.status(404).json({message:'Usuário não encontrado'})
-    }
+  const usuarioEncontrado = await Usuario.findOne({ email }).select(
+    "+password"
+  );
+  if (!usuarioEncontrado) {
+    return res.status(401).json({ error: "Email ou senha inválidos" });
   }
-  
+  if (usuarioEncontrado) {
+    const result = await bcrypt.compare(password, usuarioEncontrado.password);
+    if (!result) {
+      return res.status(401).json({ error: "Email ou senha inválidos" });
+    }
+    const token = jwt.sign(
+      {
+        id: usuarioEncontrado._id,
+        email: usuarioEncontrado.email,
+        name: usuarioEncontrado.name,
+      },
+      `${jwtKey}`,
+      { expiresIn: "1d" }
+    );
+    res.status(200).json({ token });
+  }
 });
 
 module.exports = usuario;
