108a9ffadc28817c0d19233dade199f2f10386f1
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-06-08 15:25:25 -0300

    fix: correção de segurança na rota de criação dos produtos

--- src/middlewares/compressImages.js
+++ src/middlewares/compressImages.js
@@ -1,20 +1,36 @@
 // compressImages.js
 
-import sharp from 'sharp';
-
+import sharp from "sharp";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const jwtKey = process.env.JWT_KEY;
 const compressImages = async (req, res, next) => {
-    const promises = req.files.map(async (file, index) => {
-        const compressedImage = await sharp(file.buffer).resize().webp({ quality: 60 }).toBuffer();
+  const [, token] = req.headers.authorization.split(" ");
+  const decoded = jwt.decode(token, `${jwtKey}`);
+  if (decoded.role === "admin") {
+    if (req.files) {
+      const promises = req.files.map(async (file, index) => {
+        const compressedImage = await sharp(file.buffer)
+          .resize()
+          .webp({ quality: 60 })
+          .toBuffer();
         const newFile = {
-            ...file,
-            buffer: compressedImage,
-            size: compressedImage.byteLength,
-            originalname: file.originalname.replace(/\.[^/.]+$/, ".webp"),
+          ...file,
+          buffer: compressedImage,
+          size: compressedImage.byteLength,
+          originalname: file.originalname.replace(/\.[^/.]+$/, ".webp"),
         };
         req.files[index] = newFile;
-    });
-    await Promise.all(promises);
+      });
+      await Promise.all(promises);
+      next();
+    } 
+  }
+  else {
+    res.status(401).json({ error: "Apenas administradores podem postar produtos" });
     next();
-}
+  }
+};
 
 export default compressImages;
