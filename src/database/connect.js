6231974eca4a84a3ef80043b09e1922f6281adac
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-06-08 12:19:56 -0300

    refactor: utilização do ES6

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
-const { createClient } = require("@supabase/supabase-js");
+import dotenv from "dotenv";
+dotenv.config();
+import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 const supabaseProducts = createClient(supabaseUrl, supabaseServiceKey);
-module.exports = supabaseProducts
+export default supabaseProducts
