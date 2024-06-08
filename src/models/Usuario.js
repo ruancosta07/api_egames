96d8800008d7d7ff0f4f0848796b536fac663eeb
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-06-08 12:21:20 -0300

    refactor: utilização do ES6

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,34 +1,47 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const bcrypt = require('bcrypt')
-const Usuario = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        select:false
-    },
-    role:{
-        type: String,
-        required: true,
-        default: 'client'
-    },
-    adress:[],
-    cart: [],
-    favorites:[]
-})
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+import {Schema, model} from "mongoose";
+import bcrypt from "bcrypt";
 
-Usuario.pre('save', async function(next){
-    if(this.isModified('password')) this.password = await bcrypt.hash(this.password, 12)
-        next()
-})
+const UsuarioSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "client",
+  },
+  adress: {
+    cep: { type: String },
+    logradouro: { type: String },
+    complemento: { type: String },
+    bairro: { type: String },
+    cidade: { type: String },
+    estado: { type: String },
+    numero: { type: String },
+  },
+  cart: [],
+  favorites: [],
+});
 
-module.exports = mongoose.model('usuarios', Usuario)
+UsuarioSchema.pre("save", async function (next) {
+  if (this.isModified("password"))
+    this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const Usuario = model("usuarios", UsuarioSchema);
+
+export default Usuario;
