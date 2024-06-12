fded01122ef3a8207d20fabdb1f8915f7603b635
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-06-11 22:25:56 -0300

    fix: correção na leitura do json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import express from "express"
-const app = express();
-const port = 3000;
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 import produtos from "./routes/produtos.js";
-import cors from "cors";
 import usuario from "./routes/usuario.js";
-import cookieParser from"cookie-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import compression from "compression";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+// import swaggerDocument from "../swagger.json";
+import {readFile} from "fs/promises"
+const json = JSON.parse(await readFile("./src/swagger.json", "utf8"))
+const app = express();
+const port = process.env.PORT || 3000;
+
 dotenv.config();
 app.use(compression({ level: 3 }));
 app.use(cookieParser());
@@ -19,21 +24,23 @@ app.use(
     // credentials: true,
   })
 );
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Serve a documentação Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(json));
 app.use("/", produtos);
 app.use("/", usuario);
 
 async function dbConnect() {
-  try{
+  try {
     const connection = await mongoose.connect(
       `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.sdqptiw.mongodb.net/`
     );
-    app.listen(port, () => console.log(`Server running at http://localhost:${port}, conectado ao db`))
-  }catch(error){
-    throw error
+    app.listen(port, () =>
+      console.log(`Server running at http://localhost:${port}, conectado ao db`)
+    );
+  } catch (error) {
+    throw error;
   }
 }
 
-dbConnect()
+dbConnect();
