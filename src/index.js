1ed10df12d25be0ae3f8bf67f4e62f9381e90fbf
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-06-14 20:25:18 -0300

    fix: atualização da versão do swagger

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ const options = {
 };
 const swaggerDocument = swaggerJSDoc(options);
 // Serve a documentação Swagger UI
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.css'}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css'}));
 app.use("/", produtos);
 app.use("/", usuario);
 
