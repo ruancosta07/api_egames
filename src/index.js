b8cf8ce5de54b346cd32b579e3251c58907d023b
RuanCosta07 <ruancosta.ti0805@gmail.com>
2024-09-06 20:55:42 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,25 +3,20 @@ import swaggerUi from "swagger-ui-express";
 import produtos from "./routes/produtos.js";
 import usuario from "./routes/usuario.js";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import compression from "compression";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import swaggerDocument from "../swagger.json";
-// import {readFile} from "fs/promises"
-// const json = JSON.parse(await readFile("./swagger.json", "utf8"))
 import swaggerJSDoc from "swagger-jsdoc";
 const app = express();
 const port = process.env.PORT || 3000;
 
 dotenv.config();
 app.use(compression({ level: 3 }));
-app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: ["http://localhost:3000", "http://localhost:5173", "https://ruancosta-egames-2.vercel.app"],
+    origin: "*",
     // credentials: true,
   })
 );
